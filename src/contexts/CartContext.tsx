a70a98e9264f247f81b2d9acf20c6591b06f7651
Pedro Batista <pedro@yellowberry.com.br>
2023-01-21 10:48:44 -0300

    working on context

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode, useState } from "react";
+import { Coffee } from "../pages/Home/components/Cards";
+
+export interface CartItem extends Coffee {
+  quantity: number;
+}
+
+interface CartContextType {
+  cartItems: CartItem[];
+}
+
+interface CartContextProviderProps {
+  children: ReactNode;
+}
+
+export const CartContext = createContext({} as CartContextType);
+
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  return (
+    <CartContext.Provider value={{ cartItems }}>
+      { children }
+    </CartContext.Provider>
+  )
+}
--- src/data/coffees.ts
+++ src/data/coffees.ts
@@ -5,8 +5,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "O tradicional café feito com água quente e grãos moídos",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Tradicional.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Tradicional.svg"
   },
   {
     "id": 2,
@@ -14,8 +13,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Expresso diluído, menos intenso que o tradicional",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Americano.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Americano.svg"
   },
   {
     "id": 3,
@@ -23,8 +21,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Café expresso tradicional com espuma cremosa",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Cremoso.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Cremoso.svg"
   },
   {
     "id": 4,
@@ -32,8 +29,7 @@ export const coffees = [
     "tags": ["Tradicional", "Gelado"],
     "description": "Bebida preparada com café expresso e cubos de gelo",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Gelado.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Expresso-Gelado.svg"
   },
   {
     "id": 5,
@@ -41,8 +37,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Meio a meio de expresso tradicional com leite vaporizado",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Cafe-com-Leite.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Cafe-com-Leite.svg"
   },
   {
     "id": 6,
@@ -50,8 +45,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Uma dose de café expresso com o dobro de leite e espuma cremosa",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Latte.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Latte.svg"
   },
   {
     "id": 7,
@@ -59,8 +53,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Bebida com canela feita de doses iguais de café, leite e espuma",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Capuccino.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Capuccino.svg"
   },
   {
     "id": 8,
@@ -68,8 +61,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Café expresso misturado com um pouco de leite quente e espuma",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Macchiato.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Macchiato.svg"
   },
   {
     "id": 9,
@@ -77,8 +69,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Café expresso com calda de chocolate, pouco leite e espuma",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Mocaccino.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Mocaccino.svg"
   },
   {
     "id": 10,
@@ -86,8 +77,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Bebida feita com chocolate dissolvido no leite quente e café",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Chocolate-Quente.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Chocolate-Quente.svg"
   },
   {
     "id": 11,
@@ -95,8 +85,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Drink gelado de café expresso com rum, creme de leite e hortelã",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Cubano.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Cubano.svg"
   },
   {
     "id": 12,
@@ -104,8 +93,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Bebida adocicada preparada com café e leite de coco",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Havaiano.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Havaiano.svg"
   },
   {
     "id": 13,
@@ -113,8 +101,7 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Bebida preparada com grãos de café árabe e especiarias",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Arabe.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Arabe.svg"
   },
   {
     "id": 14,
@@ -122,7 +109,6 @@ export const coffees = [
     "tags": ["Tradicional"],
     "description": "Bebida a base de café, uísque irlandês, açúcar e chantilly",
     "price": 9.90,
-    "imgUrl": "src/pages/Home/components/Cards/assets/Irlandes.svg",
-    "amount": 0
+    "imgUrl": "src/pages/Home/components/Cards/assets/Irlandes.svg"
   }
 ]
